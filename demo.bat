7d0b0d9085363fb093ca780e86c0c8fd40be63ef
Cosmic Open Source Projects <rodeboys5@gmail.com>
2015-01-09 16:39:38 -0500

    Version 1

---
+++ demo.bat
@@ -0,0 +1,9 @@
+::Version 1.0 Sample
+::When adding this code to your python program turrn it to a exe with http://bat2exe.net/
+echo off
+set /p pathName=Enter Python Version Example: if python version is 3.2 type 32!
+echo Starting!
+start c:\python%versionnum%\python run.py
+echo Boot is done...
+PAUSE
+END
